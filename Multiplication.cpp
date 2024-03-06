fa2bb23d23693059d0718a3932015b59338e90af
LALITHA-14 <147792277+LALITHA-14@users.noreply.github.com>
2024-03-06 23:14:11 +0530

    #include<iostream> using namespace std; int main() {     int num1,num2,multiplication;     cin>>num1;     cin>>num2;     multiplication=num1*num2;     cout<<multiplication;     return 0; }

---
+++ Multiplication.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num1,num2,multiplication;
+    cin>>num1;
+    cin>>num2;
+    multiplication=num1*num2;
+    cout<<multiplication;
+    return 0;
+}
