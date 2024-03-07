96386d36078fdaedf75ae9ca6b68b9101938f15b
LALITHA-14 <147792277+LALITHA-14@users.noreply.github.com>
2024-03-07 22:02:11 +0530

    #include<iostream> using namespace std; int main() {     int a,area,volume;     cin>>a;     area=6*a*a;     volume=a*a*a;     cout<<"Surface area"<<" = "<<area<<" and "<<"Volume"<<" = "<<volume;
    }

---
+++ Surface_Area_and_Volume_of_a_Cube.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,area,volume;
+    cin>>a;
+    area=6*a*a;
+    volume=a*a*a;
+    cout<<"Surface area"<<" = "<<area<<" and "<<"Volume"<<" = "<<volume;
+    
+}
