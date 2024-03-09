ff61829d7fcf5e6cb45fc6552047f98e8e62cc38
LALITHA-14 <147792277+LALITHA-14@users.noreply.github.com>
2024-03-09 21:20:53 +0530

    #include<iostream> using namespace std; int main() {     int a,b,sum,diff,pro,quo,rem;     cin>>a>>b;     sum=a+b;     diff=a-b;     pro=a*b;     quo=a/b;     rem=a%b;     cout<<"Sum:"<<sum<<" ";     cout<<"Difference:"<<diff<<" ";     cout<<"Product:"<<pro<<" ";     cout<<"Quotient:"<<quo<<" ";     cout<<"Remainder:"<<rem; }

---
+++ Arithmetic_operations.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,sum,diff,pro,quo,rem;
+    cin>>a>>b;
+    sum=a+b;
+    diff=a-b;
+    pro=a*b;
+    quo=a/b;
+    rem=a%b;
+    cout<<"Sum:"<<sum<<"
+";
+    cout<<"Difference:"<<diff<<"
+";
+    cout<<"Product:"<<pro<<"
+";
+    cout<<"Quotient:"<<quo<<"
+";
+    cout<<"Remainder:"<<rem;
+}
