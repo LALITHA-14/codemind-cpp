a077efba8af4557b95d55bd35d7414998bfab189
LALITHA-14 <147792277+LALITHA-14@users.noreply.github.com>
2024-06-12 14:45:18 +0530

    #include<bits/stdc++.h>

---
+++ Audible_Range.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int x;
+        cin>>x;
+        if(x>=67 && x<=45000){
+        printf("YES\n");
+        }
+        else{
+        printf("NO\n");
+    }
+    }
+}
